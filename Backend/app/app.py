c55c0e9527316cdabd6001da57df6c2f629e06a8
Juan Jose Mazo Acevedo <juan.mazoac@amigo.edu.co>
2023-05-06 20:03:48 -0500

    API Datos primarios2

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -11,5 +11,34 @@ def get_databases():
     rows = cursos.fetchall()
     return jsonify(rows)
 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/datosRegistro', methods=['POST'])
+def guardar_datos():
+    correo = request.json['correo']
+    date = request.json['date']
+    direccion_comments = request.json['direccion_comments']
+    direccion_number = request.json['direccion_number']
+    direccion_street = request.json['direccion_street']
+    direccion_type = request.json['direccion_type']
+    fullname = request.json['fullname']
+    genero = request.json['genero']
+    num_celular = request.json['num_celular']
+    num_fijo = request.json['num_fijo']
+    password = request.json['password']
+    
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Datos (correo, date, direccion_comments, direccion_number, direccion_street, direccion_type, fullname, genero, num_celular, num_fijo, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                   correo, date, direccion_comments, direccion_number, direccion_street, direccion_type, fullname, genero, num_celular, num_fijo, password)
+    conn.commit()
+    
+    return 'Datos guardados exitosamente'
+
+if __name__ == '__main__':
+    app.run()
+
+
 if __name__ == '__main__':
     app.run()
